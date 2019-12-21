6cc7f39b90509bb9565790571399bd0ce3aa0369
Michael <msrushton@hotmail.com>
2019-12-21 16:19:25 +0000

    Add UrnRouteConstraint

---
+++ Urn.AspNetCore.Routing/UrnRouteConstraint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Urn.AspNetCore.Routing
+{
+    /// <summary>
+    ///     A WEB API Http route constraint
+    /// </summary>
+    /// <example>
+    /// services.Configure{RouteOptions}(routeOptions =>
+    /// {
+    ///  routeOptions.ConstraintMap.Add(System.Urn.Urn.Prefix, typeof(UrnRouteConstraint));
+    /// });
+    /// ...
+    /// *Use in controller action*
+    /// [HttpPost()]
+    /// [Route("[action]/{id:urn}")]
+    /// public async  Task{IActionResult} UrnInRoute(string id)
+    /// {
+    ///  Urn urn = new Urn(id,UriFormat.UriEscaped);
+    /// }
+    /// </example>
+    public class UrnRouteConstraint : IRouteConstraint
+    {
+        /// <inheritdoc />
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            if (values.TryGetValue(routeKey, out var routeValue))
+            {
+                if (routeValue is string value)
+                {
+                    return System.Urn.Urn.IsWellFormedUrnString(value, UriFormat.Unescaped);
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
